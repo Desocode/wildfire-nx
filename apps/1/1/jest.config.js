d0081a368d881e8cfb2f0cce52db7fa398d71f99
Alex Mattera <alex.mattera@yieldify.com>
2022-01-27 14:19:04 +0000

    initial commit

---
+++ apps/1/1/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: '1-1',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/1/1',
+};
